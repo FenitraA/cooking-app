1af0d2488c6fb3f5492a4b2c0655ef936d46af2c
Rajaonarison Fenitra <fenitrandrinirina@gmail.com>
2026-09-23 17:46:56 +0300

    Backend Fix(Accidentally remove the function to get the ingredient item category)

--- cooking-app-backend/app/api/v1/endpoints/item_category_api.py
+++ cooking-app-backend/app/api/v1/endpoints/item_category_api.py
@@ -41,3 +41,13 @@ async def select_search_category(
     current_user=Depends(require_roles(settings.READ_ONLY_ROLE_DENOMINATION)),
 ):
     return await item_category_crud_instance.select_search(db, name)
+
+@router.get("/ingredient", response_model=ItemCategoryRead)
+@limiter.limit("20/minute")
+#@handle_endpoint_errors()
+async def select_search_category(
+    request: Request,
+    db: AsyncSession = Depends(get_db),
+    current_user=Depends(require_roles(settings.READ_ONLY_ROLE_DENOMINATION)),
+):
+    return await item_category_crud_instance.get_ingredient_category(db)
